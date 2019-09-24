fb090e90762463ff8f9827828b7257392ec33b52
jjungeun <jungeun9729@gmail.com>
2019-09-24 18:02:59 +0900

    config error수정

--- shared/firebaseConfig.js
+++ shared/firebaseConfig.js
@@ -1,4 +1,4 @@
-export default firebaseConfig = {
+const firebaseConfig = {
   apiKey: "AIzaSyB9P5gvuIxEcBVmiD0IOlJXloXcUUdA5Ws",
   authDomain: "insightpage-78483.firebaseapp.com",
   databaseURL: "https://insightpage-78483.firebaseio.com",
@@ -7,3 +7,5 @@ export default firebaseConfig = {
   messagingSenderId: "102059894468",
   appId: "1:102059894468:web:7eec5abb6c8bb42bb20b91"
 };
+
+export default firebaseConfig;
